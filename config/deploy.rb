7d2a4f121702cf8db24b34df007f5e88f77e484c
Pete Bevin <pete@petebevin.com>
2009-04-09 17:07:24 -0400

    Set up Capistrano; unpacked faker gem

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "bcmets"
+set :repository,  "pete@petebevin.com:git/bcmets"
+set :user, "pete"
+set :use_sudo, false
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/www/#{application}-new"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "petebevin.com"
+role :web, "petebevin.com"
+role :db,  "petebevin.com", :primary => true
