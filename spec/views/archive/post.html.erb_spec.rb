f9e25b72d094ac9d76f02525cde7424bb3f2fb26
Pete Bevin <pete@petebevin.com>
2009-04-15 21:37:04 -0400

    Removed debug output

--- spec/views/archive/post.html.erb_spec.rb
+++ spec/views/archive/post.html.erb_spec.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/archive/post" do
   it "should have fields" do
     render 'archive/post'
-    puts response.body
     response.should have_tag("form#post_form")
     response.should have_tag("input[name=?]", "article[name]")
     response.should have_tag("input[name=?]", "article[email]")
