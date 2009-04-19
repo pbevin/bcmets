1b663e406e176fc9338d5b3e3afb17fe67224b0f
Pete Bevin <pete@petebevin.com>
2009-04-18 21:42:08 -0400

    Better text formatting

--- spec/helpers/archive_helper_spec.rb
+++ spec/helpers/archive_helper_spec.rb
@@ -3,11 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ArchiveHelper do
   describe "to_html" do
     it "should protect angle brackets" do
-      helper.to_html("The <best> plan is...").should == '<pre>The &lt;best&gt; plan is...</pre>'
+      helper.to_html("The <best> plan is...").should == '<p>The &lt;best&gt; plan is...</p>'
     end
     
     it "should link email addresses" do
-      helper.to_html('pete@petebevin.com').should == '<pre><a href="mailto:pete@petebevin.com">pete@petebevin.com</a></pre>'
+      helper.to_html('pete@petebevin.com').should == '<p><a href="mailto:pete@petebevin.com">pete@petebevin.com</a></p>'
     end
     
     it "should wrap long lines"
