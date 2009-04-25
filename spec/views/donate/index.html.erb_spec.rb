00b1c3b5630620f716eca70579492110b6fbd9b8
Pete Bevin <pete@petebevin.com>
2009-04-24 20:08:50 -0400

    Fixed scaffold test

--- spec/views/donate/index.html.erb_spec.rb
+++ spec/views/donate/index.html.erb_spec.rb
@@ -5,8 +5,7 @@ describe "/donate/index" do
     render 'donate/index'
   end
   
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/donate/index])
+  it "should link to paypal" do
+    response.should have_tag("a[href^=https://www.paypal.com/]")
   end
 end
