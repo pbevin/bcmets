67d7a6958c8acfd8a64191abe5a81274e78c796e
Pete Bevin <pete@petebevin.com>
2009-10-28 10:25:19 -0400

    Added authlogic config for production oops

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ require 'maxmind'
 
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
+    c.login_field = :email
     c.validates_length_of_password_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
     c.validates_length_of_password_confirmation_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
   end
