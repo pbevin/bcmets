1d0ef3c1323f732999907719c05e6799d17b277b
Pete Bevin <pete@petebevin.com>
2010-02-28 22:18:54 -0500

    Oops, user images display properly now

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def profile_picture(user, size = :medium)
     if user.photo.file?
-      image_tag user.photo.url(:size), :class => 'profile'
+      image_tag user.photo.url(size), :class => 'profile'
     else
       gravatar_for user, :default => 'identicon', :size => 50, :class => 'profile'
     end
