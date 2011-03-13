452a03c48bf0ff4a5976672c61b5d3418ab206f2
Pete Bevin <pete@petebevin.com>
2011-03-12 19:31:07 -0500

    Set domain on session cookie
    This should finally fix those InvalidAuthenticityToken errors.
    The problem was, as far as I can tell, that the user was getting
    two different sessions, each with its own authenticity token,
    and the wrong one was getting picked up.  http://is.gd/A1yAay
    (stack overflow) has some clues on why this happens.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,11 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_bcmets_session',
+  :domain      => 'bcmets.org',
   :secret      => '3f48631d6753e45e4a26ed3096558feabbc20e3216d2944cc297da769ffcc12118f2f034c4d64cccebb83ae6340c27f2ba6da61a651023bda263438cb59deb8b'
 }
 
