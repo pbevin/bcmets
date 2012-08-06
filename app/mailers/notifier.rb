ed119999b9512b4c60a78a9fb178ae859b6d6db3
Pete Bevin <pete@petebevin.com>
2012-08-06 16:46:57 -0400

    Fix mail content type and dev settings

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,17 +3,17 @@ class Notifier < ActionMailer::Base
 
   def activation_instructions(user)
     @account_activation_url = register_url(user.perishable_token)
-    mail(:subject => "Activation Instructions", :recipients => user.email)
+    mail(:subject => "Activation Instructions", :to => user.email)
   end
 
   def activation_confirmation(user)
     @root_url = root_url
-    mail(:subject => "Activation Complete", :recipients => user.email)
+    mail(:subject => "Activation Complete", :to => user.email)
   end
 
   def password_reset(user)
     @reset_password_url = user.perishable_token
-    mail(:subject => "Password reset", :recipients => user.email)
+    mail(:subject => "Password reset", :to => user.email)
   end
 
   def article(article)
@@ -23,10 +23,10 @@ class Notifier < ActionMailer::Base
     @article = article
 
     mail(
-      :subject    => article.subject,
-      :recipients => article.mail_to,
-      :cc         => article.mail_cc,
-      :from       => article.from
+      :subject => article.subject,
+      :to      => article.mail_to,
+      :cc      => article.mail_cc,
+      :from    => article.from
     )
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,21 @@ Bcmets::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  $list_address = 'pete@petebevin.com'
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'bcmets.org',
+    :user_name            => 'pbevin@gmail.com',
+    :password             => File.read(".passwd-gmail").strip,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 end
