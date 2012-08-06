1acb2239768c75819c00cd086fd670568ebd8caf
Pete Bevin <pete@petebevin.com>
2012-08-06 15:26:27 -0400

    Restore user photos to profile picture

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,14 @@
 module UsersHelper
   def profile_picture(user, size = :medium)
-#   if user.photo.file?
-#     image_tag user.photo.url(size), :class => 'profile'
-#   else
-#     pixel_size = 100 # always for gravatar - they look silly at larger sizes.
-#     gravatar_for user, :default => 'identicon', :size => pixel_size, :class => 'profile'
-#   end
-    options = {
-      :default => "identicon",
-      :size => 100
-    }
-    gravatar_image_tag(user.email, :class => "profile", :gravatar => options)
+    if user.photo.file?
+      image_tag user.photo.url(size), :class => 'profile'
+    else
+      options = {
+        :default => "identicon",
+        :size => 100
+      }
+      gravatar_image_tag(user.email, :class => "profile", :gravatar => options)
+    end
   end
 
   def profile_picture_linked(user)
