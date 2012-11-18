86edc93c533b5ce9aac4411e67997d37e95e5573
Pete Bevin <pete@petebevin.com>
2012-11-18 15:29:05 -0500

    Show tooltip when hovering on article star

---
+++ app/assets/javascripts/bcmets.js
@@ -0,0 +1,44 @@
+function initStar() {
+  var status = $("#header .status");
+  status.hide();
+  $("#star a").click( function() {
+    var star = $(this);
+    var spinner = star.parent().find("img");
+    var ajaxDone = function() { spinner.hide(); star.show(); };
+    var saved = false;
+
+    star.toggleClass("selected");
+    if (star.hasClass("selected")) {
+      saved = true;
+    }
+    star.hide(); spinner.show();
+    $.ajax({
+      type: "POST",
+      url: document.location.href + "/set_saved",
+      data: { saved: saved },
+      success: ajaxDone,
+      error: ajaxDone
+    });
+  } );
+
+  function setStatus(message) {
+    if (message != "") {
+      status.text(message).fadeIn();
+    } else {
+      status.fadeOut();
+    }
+  }
+  function clearStatus() {
+    setStatus("");
+  }
+  clearStatus();
+
+  $("#star a").hover(function() {
+    var star = $(this);
+    if (star.hasClass("selected")) {
+      setStatus("Article is in your saved list. Click again to remove it.");
+    } else {
+      setStatus("Click the star to add this article to your Saved Articles list.");
+    }
+  }, clearStatus);
+}
