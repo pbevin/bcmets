eadb4c2b2b0944665bd3e4f944abd81a41954353
Pete Bevin <pete@petebevin.com>
2013-10-26 17:36:45 -0400

    Split off ArticleBodyFormatter

---
+++ lib/article_body_formatter.rb
@@ -0,0 +1,32 @@
+class ArticleBodyFormatter
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::UrlHelper
+
+  def h(text)
+    ERB::Util.html_escape(text)
+  end
+
+  def format(text)
+    auto_link(simple_format(h(decode_quoted_printable(text))))
+  end
+
+  def decode_quoted_printable(text)
+    if (text =~ /=20$/)
+      text = text
+        .gsub(/=20\n/, " ")
+        .gsub(/=\n/, "")
+        .gsub(/\n/m, $/)
+        .gsub(/=([\dA-F]{2})/) { hex_to_utf8($1.hex) }
+    end
+
+    return text
+  end
+
+  def hex_to_utf8(code)
+    # 99% of the time, code is 0x20 (space) or 0x3D (equal sign).
+    # But sometimes, it's a funny character, which usually comes from
+    # silly Windows clients.  So we assume ISO8859-1, but return it
+    # encoded as UTF-8.
+    code.chr.force_encoding(Encoding::ISO8859_1).encode(Encoding::UTF_8)
+  end
+end
