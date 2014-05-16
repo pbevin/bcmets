d88b0df63315980bd0a0231e5ec021e732ee833c
Pete Bevin <pete@hover.com>
2014-05-16 15:19:37 -0400

    Upgrade user passwords to scrypt as they login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ require 'maxmind'
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = :email
+    c.transition_from_crypto_providers = Authlogic::CryptoProviders::Sha512,
     c.validates_length_of_password_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
     c.validates_length_of_password_confirmation_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
   end
