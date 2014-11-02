ce94759397d90547a1fc15f2c9e4386cfbb17071
Pete Bevin <pete@petebevin.com>
2014-11-02 09:49:45 -0500

    Refactor UpdateFeeds task

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -1,5 +1,6 @@
+# Action for 'rake feeds'.  Grabs XML URL for any feeds that don't have them,
+# then grabs new RSS entries.
 class UpdateFeeds
-
   def self.run
     new.run
   end
@@ -15,31 +16,38 @@ class UpdateFeeds
 
   def update_entries(feed)
     feed_url = feed.xml_url
-    begin
-      Feed.transaction do
-        rss = Feedjira::Feed.fetch_and_parse(feed_url)
-        return if rss.nil?
-        rss.entries.each do |entry|
-          feed.add_entry(FeedEntry.from_rss_entry(entry))
-        end
+    Feed.transaction do
+      rss = Feedjira::Feed.fetch_and_parse(feed_url)
+      return if rss.nil?
+      rss.entries.each do |entry|
+        feed.add_entry(FeedEntry.from_rss_entry(entry))
       end
-    rescue => e
-      Rails.logger.warn("Failed to fetch #{feed_url}: #{e.inspect}")
     end
+  rescue => e
+    Rails.logger.warn("Failed to fetch #{feed_url}: #{e.inspect}")
   end
 
   def update_xml_url(feed)
-    content = URI.parse(feed.url).read
-    if content.content_type == 'text/html'
+    fetching(feed.url) do |content|
       doc = Nokogiri::HTML(content)
-      if node = doc.css('link[rel=alternate][href]').first
-        href = node['href']
-        title = Feedjira::Feed.fetch_and_parse(href).title
-        feed.update_attributes(
-          xml_url: href,
-          name: title
-        )
-      end
+      node = doc.css('link[rel=alternate][href]').first
+      update_feed(node, feed) if node
     end
+  end
+
+  private
+
+  def update_feed(node, feed)
+    href = node['href']
+    title = Feedjira::Feed.fetch_and_parse(href).title
+    feed.update_attributes(
+      xml_url: href,
+      name: title
+    )
+  end
+
+  def fetching(url)
+    content = URI.parse(url).read
+    yield content if content.content_type == 'text/html'
   end
 end
