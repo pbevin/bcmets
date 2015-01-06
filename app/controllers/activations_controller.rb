4eb5dde79d0f9b88b30527e6b45d69a3fe44047f
Pete Bevin <pete@petebevin.com>
2015-01-06 11:01:16 -0500

    Fix html_safe for Already Activated flash

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -2,10 +2,11 @@ class ActivationsController < ApplicationController
   def new
     @user = User.find_using_perishable_token(params[:activation_code], 1.week)
     if !@user
-      flash[:notice] = "You have already activated your account."
+      message = "You have already activated your account."
       if !current_user
-        flash[:notice] += " " + self.class.helpers.link_to("Click here to login", login_url)
+        message += " " + self.class.helpers.link_to("Click here to login", login_url)
       end
+      flash[:notice] = message.html_safe
       redirect_to root_url
     end
   end
