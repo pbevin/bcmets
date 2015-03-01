4691df80f21adf41f0cf8dfc1409f10ea8086859
Pete Bevin <pete@petebevin.com>
2015-03-01 11:00:16 -0500

    Blockquote quoted text in timeline articles

--- lib/article_body_compact_formatter.rb
+++ lib/article_body_compact_formatter.rb
@@ -1,12 +1,54 @@
 class ArticleBodyCompactFormatter < ArticleBodyFormatter
   def format(text)
+    text = correct_newlines(text)
+    text = sanitize(text)
     text = remove_signature_block(text)
+    text = remove_attachment_warnings(text)
     text = decode_quoted_printable(text)
 
-    auto_link(simple_format(h(text)))
+    auto_link(article_format(h(text)))
+  end
+
+  def correct_newlines(text)
+    text.gsub(/\r\n?/, "\n")
+  end
+
+  def remove_attachment_warnings(text)
+    text.gsub(%r{\[list software deleted \w+/\w+ attachment\]\n*}, "")
   end
 
   def remove_signature_block(text)
     text.gsub(/^--\s*\n(.*\n){,5}.*\s*\Z/, '').strip
+  end
+
+
+  def article_format(text)
+    paragraphs = text.split(/\n{2,}/)
+
+    if paragraphs.empty?
+      content_tag("p", nil)
+    else
+      paragraphs.map! { |paragraph|
+        if paragraph =~ /^&gt;/
+          paragraph = paragraph.gsub(/^&gt;/, '')
+          content_tag("blockquote", article_format(paragraph))
+        else
+          format_para(paragraph)
+        end
+      }.join("\n\n").html_safe.tap { |p| p p }
+    end
+  end
+
+  def format_para(paragraph)
+    paragraph = paragraph.gsub(/([^\n]\n)(?=[^\n])/, '\1<br />')
+    content_tag("p", raw(paragraph))
+  end
+
+  def split_paragraphs(text)
+    return [] if text.blank?
+
+    text.split(/\n{2,}/).map! do |t|
+      t.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') || t
+    end
   end
 end
--- spec/lib/article_body_compact_formatter_spec.rb
+++ spec/lib/article_body_compact_formatter_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe ArticleBodyCompactFormatter do
+  let(:formatter) { ArticleBodyCompactFormatter.new }
 
   describe '#remove_signature_block' do
-    let(:formatter) { ArticleBodyCompactFormatter.new }
     let(:text) {
       [
         "This part should stay\n",
@@ -22,4 +22,26 @@ describe ArticleBodyCompactFormatter do
         .should == "This part should stay"
     end
   end
+
+
+  describe '#remove_attachment_warnings' do
+    it "removes a text/html warning" do
+      formatter.remove_attachment_warnings([
+        "This part should stay\n",
+        "\n",
+        "[list software deleted text/html attachment]\n",
+        "\n"
+      ].join).should == "This part should stay\n\n"
+    end
+
+    it "works if the attachment is at the end of the string" do
+      formatter.remove_attachment_warnings([
+        "This part should stay\n",
+        "\n",
+        "[list software deleted text/html attachment]",
+      ].join).should == "This part should stay\n\n"
+    end
+
+  end
+
 end
