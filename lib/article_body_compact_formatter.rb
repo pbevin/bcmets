e7808e5a61901c26ea1a5124334a271b08fdc22a
Pete Bevin <pete@hover.com>
2015-03-02 10:14:47 -0500

    Small fix for multi-level quote formatting

--- lib/article_body_compact_formatter.rb
+++ lib/article_body_compact_formatter.rb
@@ -30,26 +30,14 @@ class ArticleBodyCompactFormatter < ArticleBodyFormatter
       content_tag("p", nil)
     else
       paragraphs.map! { |paragraph|
-        if paragraph =~ /^>/
-          paragraph = paragraph.gsub(/^>\s*/, '')
+        if paragraph =~ /\A>/
+          paragraph = paragraph.gsub(/^>[^\S\n]*/, '')
           content_tag("blockquote", article_format(paragraph))
         else
-          format_para(paragraph)
+          paragraph = paragraph.gsub(/([^\n]\n)(?=[^\n])/, '\1<br />')
+          content_tag("p", raw(paragraph))
         end
       }.join("\n\n").html_safe
-    end
-  end
-
-  def format_para(paragraph)
-    paragraph = paragraph.gsub(/([^\n]\n)(?=[^\n])/, '\1<br />')
-    content_tag("p", raw(paragraph))
-  end
-
-  def split_paragraphs(text)
-    return [] if text.blank?
-
-    text.split(/\n{2,}/).map! do |t|
-      t.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') || t
     end
   end
 end
