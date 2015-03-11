7c2a74c15940533734e345f124ad822c182e8e27
Pete Bevin <pete@petebevin.com>
2015-03-11 17:22:27 -0400

    Remove gravatar from server-side
    The server now returns article.avatar_url if there is a locally
    available photo, otherwise nil. It's now the client that is responsible
    for defaulting the image.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,33 +1,10 @@
 module UsersHelper
-  def profile_picture(user, size = :medium)
-    if user.photo.file?
-      image_tag user.photo.url(size), class: 'profile'
-    else
-      options = {
-        default: "identicon",
-        size: 100
-      }
-      gravatar_image_tag(user.email, class: "profile", gravatar: options)
-    end
-  end
-
   def profile_picture_url(user)
-    avatar_url(user, :small) || gravatar_url(user)
-  end
-
-  def avatar_url(user, size = :medium)
+    return nil if !user
     if user && user.photo.file?
-      user.photo.url(size)
+      user.photo.url(:small)
     else
       nil
     end
-  end
-
-  def gravatar_url(user)
-    gravatar_image_url(user.email, default: "identicon", size: 100)
-  end
-
-  def profile_picture_linked(user)
-    link_to(profile_picture(user, :small), user, class: 'profile')
   end
 end
