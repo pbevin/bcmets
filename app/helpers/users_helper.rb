b2ef5b489a6ddccac4d0db1a9e3d127cadc4fa95
Pete Bevin <pete@petebevin.com>
2015-03-12 15:05:18 -0400

    Don't display missing profile photos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,18 @@
 module UsersHelper
   def profile_picture_url(user)
     return nil if !user
-    if user && user.photo.file?
+    if user && user.photo.file? && File.exists?(user.photo.path)
       user.photo.url(:small)
     else
       nil
+    end
+  end
+
+  def profile_picture(user, size = :medium)
+    if user && user.photo.file? && File.exists?(user.photo.path)
+      image_tag user.photo.url(size), class: 'profile'
+    else
+      ""
     end
   end
 end
