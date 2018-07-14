1eda4d12e1b19303d320b01ce3aedb68ac4204c5
Pete Bevin <pete@petebevin.com>
2018-07-14 16:14:43 -0400

    Allow replies to Carol's original article

--- app/assets/javascripts/components/article.jsx
+++ app/assets/javascripts/components/article.jsx
@@ -16,9 +16,9 @@ var Article = React.createClass({
   propTypes: {
     article: React.PropTypes.object.isRequired,
     signedIn: React.PropTypes.bool.isRequired,
-    roots: React.PropTypes.array.isRequired
+    roots: React.PropTypes.array.isRequired,
   },
-  mixins : [ PureRenderMixin ],
+  mixins: [PureRenderMixin],
 
   render: function() {
     var article = this.props.article;
@@ -30,7 +30,7 @@ var Article = React.createClass({
         <Body article={article} roots={this.props.roots} />
       </div>
     );
-  }
+  },
 });
 
 var Header = React.createClass({
@@ -38,7 +38,7 @@ var Header = React.createClass({
     article: React.PropTypes.object.isRequired,
     signedIn: React.PropTypes.bool.isRequired,
   },
-  mixins : [ PureRenderMixin ],
+  mixins: [PureRenderMixin],
 
   render: function() {
     var article = this.props.article;
@@ -46,12 +46,16 @@ var Header = React.createClass({
     var star, reply;
 
     if (signedIn) {
-      star = <Star article={this.props.article} />
+      star = <Star article={this.props.article} />;
     }
 
-    if (isRecent(article.sent_at)) {
-      var replyPath = "/article/" + article.id + "/reply";
-      reply = <div id="reply_to"><a href={replyPath}>Reply to this message</a></div>;
+    if (article.id === 180601 || isRecent(article.sent_at)) {
+      var replyPath = '/article/' + article.id + '/reply';
+      reply = (
+        <div id="reply_to">
+          <a href={replyPath}>Reply to this message</a>
+        </div>
+      );
     }
 
     return (
@@ -66,7 +70,7 @@ var Header = React.createClass({
         </ul>
       </div>
     );
-  }
+  },
 });
 
 var Star = React.createClass({
@@ -80,15 +84,29 @@ var Star = React.createClass({
     if (article.saved) {
       return (
         <div id="star">
-          <a className="save_this" href="javascript:void(0)" onClick={this.toggleSaved}>Message saved</a>
+          <a
+            className="save_this"
+            href="javascript:void(0)"
+            onClick={this.toggleSaved}>
+            Message saved
+          </a>
           &nbsp;
-          <a className="star selected" href="javascript:void(0)" onClick={this.toggleSaved} />
+          <a
+            className="star selected"
+            href="javascript:void(0)"
+            onClick={this.toggleSaved}
+          />
         </div>
       );
     } else {
       return (
         <div id="star">
-          <a className="save_this" href="javascript:void(0)" onClick={this.toggleSaved}>Save this message</a>
+          <a
+            className="save_this"
+            href="javascript:void(0)"
+            onClick={this.toggleSaved}>
+            Save this message
+          </a>
           &nbsp;
           <a className="star" href="javascript:void(0)" onClick={this.toggleSaved} />
         </div>
@@ -98,46 +116,50 @@ var Star = React.createClass({
 
   toggleSaved: function() {
     Actions.toggleSaved();
-  }
-
+  },
 });
 
 var Body = React.createClass({
   propTypes: {
     article: React.PropTypes.object.isRequired,
-    roots: React.PropTypes.array.isRequired
+    roots: React.PropTypes.array.isRequired,
   },
-  mixins : [ PureRenderMixin ],
+  mixins: [PureRenderMixin],
 
   render: function() {
     article = this.props.article;
     return (
       <div id="body">
         <Avatar article={article} />
-        <div dangerouslySetInnerHTML={{__html: article.body }} />
+        <div dangerouslySetInnerHTML={{__html: article.body}} />
         <div className="thread">
           <h2>Articles in this thread</h2>
           <ConversationLinks roots={this.props.roots} />
         </div>
       </div>
-    )
-  }
+    );
+  },
 });
 
 var ConversationLinks = React.createClass({
   propTypes: {
-    roots: React.PropTypes.array.isRequired
+    roots: React.PropTypes.array.isRequired,
   },
-  mixins : [ PureRenderMixin ],
+  mixins: [PureRenderMixin],
 
   render: function() {
     var links = this.props.roots.map(function(article) {
-      var href = "/articles/" + article.id;
-      var children = article.children.length > 0 ? <ConversationLinks roots={article.children} /> : null;
-      var spc = " ";
+      var href = '/articles/' + article.id;
+      var children =
+        article.children.length > 0 ? (
+          <ConversationLinks roots={article.children} />
+        ) : null;
+      var spc = ' ';
       return (
         <li key={article.id}>
-          <a href={href} className="subject">{article.subject}</a>
+          <a href={href} className="subject">
+            {article.subject}
+          </a>
           {spc}
           <small>
             {linkToAuthor(article.sender_name, article.sender_email)},
@@ -149,42 +171,48 @@ var ConversationLinks = React.createClass({
       );
     });
 
-    return (
-      <ul className="conversation">
-        {links}
-      </ul>
-    );
-  }
+    return <ul className="conversation">{links}</ul>;
+  },
 });
 
 function isRecent(date) {
-  return moment(date).isAfter(moment().subtract(1, "month"));
+  return moment(date).isAfter(moment().subtract(1, 'month'));
 }
 
 function dateAndTimeAgo(sentAtDate) {
   var date = moment(sentAtDate);
-  return humanDate(date) + " (" + timeAgo(date) + ")";
+  return humanDate(date) + ' (' + timeAgo(date) + ')';
 }
 
-function humanDate(date) { // date is a moment
+function humanDate(date) {
+  // date is a moment
   var startOfToday = moment().startOf('day');
   if (date.isBefore(startOfToday)) {
-    return date.format("MMM D, YYYY");
+    return date.format('MMM D, YYYY');
   } else {
-    return date.format("hh:mm A");
+    return date.format('hh:mm A');
   }
 }
 
-function timeAgo(date) { // date is a moment
+function timeAgo(date) {
+  // date is a moment
   return date.fromNow();
 }
 
 function linkToAuthor(sender_name, sender_email) {
-  var path = "/archive/author?email=" + sender_email;
+  var path = '/archive/author?email=' + sender_email;
 
   if (sender_name) {
-    return <span>{sender_name} &lt;<a href={path}>{sender_email}</a>&gt;</span>;
+    return (
+      <span>
+        {sender_name} &lt;<a href={path}>{sender_email}</a>&gt;
+      </span>
+    );
   } else {
-    return <span>&lt;<a href={path}>{sender_email}</a>&gt;</span>;
+    return (
+      <span>
+        &lt;<a href={path}>{sender_email}</a>&gt;
+      </span>
+    );
   }
 }
